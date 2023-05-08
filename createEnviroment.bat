58495ab303c52433d6d4591178a7e0f60f760046
Angas96 <antonio.gasparevic@gmail.com>
2023-05-08 18:02:42 +0200

    Update createEnviroment.bat

--- createEnviroment.bat
+++ createEnviroment.bat
@@ -5,7 +5,7 @@ set POSTGRES_USER=postgres
 set POSTGRES_PASSWORD=postgres
 set POSTGRES_DB=AutomatedPostgres
 set POSTGRES_PORT=5434
-set POSTGRES_OPENWEATHERLINK="https://api.openweathermap.org/data/2.5/weather?lat=47.4667&lon=13.25&appid=9baa53afdf2ddb2f93e029e19ff7e904"
+set POSTGRES_OPENWEATHERLINK="https://api.openweathermap.org/data/2.5/weather?q=Werfenweng&units=metric&appid=9baa53afdf2ddb2f93e029e19ff7e904"
 
 REM Build the Docker image with the Dockerfile
 docker build -t automatedpostgress .
