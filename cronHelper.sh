343885ae42e10f1372b28ceffc95d90409728f90
Angas96 <antonio.gasparevic@gmail.com>
2023-06-29 01:09:46 +0200

    Delete cronHelper.sh

--- cronHelper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sleep 15
-docker exec -it database-automatedpostgres-1 /bin/bash -c "bash /docker-entrypoint-initdb.d/entry.sh"
-docker exec -it database-automatedpostgres-1 /bin/bash -c "bash /helperScripts/cronStart.sh"
