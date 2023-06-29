0d4a0d7852b7eb9cb4932b77220d0dc4eb50862c
Angas96 <antonio.gasparevic@gmail.com>
2023-06-29 09:38:38 +0200

    Update linuxPermission.sh

--- linuxPermission.sh
+++ linuxPermission.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-chmod +x -R /sql
-chmod +x -R /helperScripts
-chmod +x -R /wetter
-chmod +x -R /database
+chmod +x -R sql
+chmod +x -R helperScripts
+chmod +x -R wetter
+chmod +x -R database
 chmod +x restartContainer.sh
