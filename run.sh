8e0c79c8ac216d88844752ed8f7baa0208df47f4
Angas96 <antonio.gasparevic@gmail.com>
2023-07-06 11:35:36 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@ docker network create pgnetwork
 docker volume create postgres-data
 docker volume create pgadmin-data
 docker compose up -d
+cd ..
+./linuxPermission.sh
 sleep 15
 docker exec -it database-automatedpostgres-1 /bin/bash -c "bash /sql/firstStart.sh"
 docker exec -it database-automatedpostgres-1 /bin/bash -c "bash /sql/entry.sh"
 docker exec -it database-automatedpostgres-1 /bin/bash -c "bash /helperScripts/cronStart.sh"
-cd ..
