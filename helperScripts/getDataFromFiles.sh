82e7ca3a301627357b23dd1ad8481e37116b24c7
Angas96 <antonio.gasparevic@gmail.com>
2023-07-09 20:57:40 +0200

    Update getDataFromFiles.sh

--- helperScripts/getDataFromFiles.sh
+++ helperScripts/getDataFromFiles.sh
@@ -2,14 +2,14 @@
 
 sleep 5
 
-psql -U postgres -d AutomatedPostgres -c "select import_percipitation('/jsonfiles/Werfenweng_MetGIS_Precipitation_hourly_20200901-20201101.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_percipitation('/jsonfiles/Werfenweng_MetGIS_Precipitation_hourly_20211201-20220301.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_relhumidity('/jsonfiles/Werfenweng_MetGIS_RelHumidity_hourly_20200901-20201101.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_relhumidity('/jsonfiles/Werfenweng_MetGIS_RelHumidity_hourly_20211201-20220301.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_temperature('/jsonfiles/Werfenweng_MetGIS_Temperature_hourly_20200901-20201101.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_temperature('/jsonfiles/Werfenweng_MetGIS_Temperature_hourly_20211201-20220301.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_wind('/jsonfiles/Werfenweng_MetGIS_Wind_hourly_20200901-20201101.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_wind('/jsonfiles/Werfenweng_MetGIS_Wind_hourly_20211201-20220301.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_country('/jsonfiles/zipcodes.at.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_country('/jsonfiles/zipcodes.de.json')"
-psql -U postgres -d AutomatedPostgres -c "select import_country('/jsonfiles/zipcodes.ch.json')"
+psql -U postgres -d AutomatedPostgres -c "select import_percipitation('/jsonfiles/Werfenweng_MetGIS_Precipitation_hourly_20200901-20201101.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_percipitation('/jsonfiles/Werfenweng_MetGIS_Precipitation_hourly_20211201-20220301.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_relhumidity('/jsonfiles/Werfenweng_MetGIS_RelHumidity_hourly_20200901-20201101.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_relhumidity('/jsonfiles/Werfenweng_MetGIS_RelHumidity_hourly_20211201-20220301.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_temperature('/jsonfiles/Werfenweng_MetGIS_Temperature_hourly_20200901-20201101.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_temperature('/jsonfiles/Werfenweng_MetGIS_Temperature_hourly_20211201-20220301.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_wind('/jsonfiles/Werfenweng_MetGIS_Wind_hourly_20200901-20201101.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_wind('/jsonfiles/Werfenweng_MetGIS_Wind_hourly_20211201-20220301.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_country('/jsonfiles/zipcodes.at.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_country('/jsonfiles/zipcodes.de.json')" >> status.txt
+psql -U postgres -d AutomatedPostgres -c "select import_country('/jsonfiles/zipcodes.ch.json')" >> status.txt
